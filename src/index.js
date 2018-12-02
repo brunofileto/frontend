8fa787ed86a9dde87cdb5319dfa360880ec4d9cf
hiagoBonamelli <hiagohg@hotmail.com>
2018-12-02 16:50:23 -0200

    Revert "tentativa bootstrap"
    This reverts commit 90a41a879d8193dcff8702c95c0c0517e84e8709.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
 
-// Import bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const root = document.getElementById('root');
 
 if (root !== null) {
