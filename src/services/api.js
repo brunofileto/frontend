db05c6414dcfc0cf9f6f3a910e57bf8fb0ae6f2d
hiagoBonamelli <hiagohg@hotmail.com>
2018-12-15 18:41:53 -0200

    alteracao no ip do django
    quando for trabalhar local tem que alterar o ip do backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: " http://34.224.173.214"
+  // baseURL: "http://34.224.173.214"
+  baseURL: "http://127.0.0.1:8000"
 });
 
 api.interceptors.request.use(function (config) {
