266e7b8f13fe5895cf47195c2f87287a0956c1c2
hiagoBonamelli <hiagohg@hotmail.com>
2018-12-28 11:48:44 -0200

    remove enrollment no logout

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,3 +1,5 @@
+import { removemEnrollment } from './storage';
+
 export const TOKEN_KEY = "TOKEN_KEY";
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
@@ -6,4 +8,5 @@ export const login = (token : string) => {
 };
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
+  removemEnrollment();
 };
--- src/services/storage.js
+++ src/services/storage.js
@@ -3,3 +3,6 @@ export const setEnrollment = (enrollment : string) => {
   localStorage.setItem(ENROLLMENT_KEY, enrollment);
 };
 export const getEnrollment = () => localStorage.getItem(ENROLLMENT_KEY);
+export const removemEnrollment = () => {
+  localStorage.removeItem(ENROLLMENT_KEY);
+};
